c6c9234365e5aaeb61c8a9846d5eaa34e08246ff
AstridVides-cyber <vkirito67@gmail.com>
2026-04-20 17:37:16 -0600

    feat: Entidad servicios

--- Laboratorio_1/src/main/java/com/example/laboratorio_1/services/ProductService.java
+++ Laboratorio_1/src/main/java/com/example/laboratorio_1/services/ProductService.java
@@ -1,4 +1,21 @@
 package com.example.laboratorio_1.services;
 
+import com.example.laboratorio_1.domain.entitie.Product;
+import com.example.laboratorio_1.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<Product> getProduct(){
+        return  productRepository.getProducts();
+    }
+
+
 }
+
