b2b89763a4459e52cce60f822c9abccc5bb0fae9
AstridVides-cyber <vkirito67@gmail.com>
2026-05-04 18:37:05 -0600

    feat: Entidad producto

---
+++ Laboratorio_2/src/main/java/com/example/laboratorio_2/domain/enttity/Product.java
@@ -0,0 +1,39 @@
+package com.example.laboratorio_2.domain.enttity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.UUID;
+
+@Entity
+@Table(name = "Product")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "productName")
+    private String name;
+
+    @Column(name = "grade")
+    private String grade;
+
+    @Column(name = "cursedTechnique")
+    private String cursedTechnique;
+
+    @Column(name = "school")
+    private String school;
+
+    @Column(name = "cursedEnergy")
+    private Double cursedEnergy;
+
+    @Column(name = "isActive")
+    private Boolean isActive;
+
+}
