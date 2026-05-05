e96fc0cf1fdfd714391556ec4f8e8208c0919cba
AstridVides-cyber <vkirito67@gmail.com>
2026-05-04 23:40:47 -0600

    feat: Entidad de repositorio

--- Laboratorio_2/src/main/java/com/example/laboratorio_2/domain/enttity/Product.java
+++ Laboratorio_2/src/main/java/com/example/laboratorio_2/domain/enttity/Product.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 import java.util.UUID;
 
 @Entity
-@Table(name = "Product")
+@Table(name = "sorcerers")
 @Data
 @Builder
 @AllArgsConstructor
---
+++ Laboratorio_2/src/main/java/com/example/laboratorio_2/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.laboratorio_2.repository;
+
+import com.example.laboratorio_2.domain.enttity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+}
