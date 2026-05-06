7d0649ae42973cc2a108621241cb026674bfdeee
AstridVides-cyber <vkirito67@gmail.com>
2026-05-05 23:01:09 -0600

    feat: Entidades servicio

---
+++ Laboratorio_2/src/main/java/com/example/laboratorio_2/service/ProductService.java
@@ -0,0 +1,7 @@
+package com.example.laboratorio_2.service;
+
+import com.example.laboratorio_2.domain.enttity.Product;
+
+public interface ProductService {
+    void createProduct(Product product);
+}
