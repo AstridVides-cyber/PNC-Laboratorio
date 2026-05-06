6d1ff69146e24136d1c6a4d3fe9b074e0be4aad8
AstridVides-cyber <vkirito67@gmail.com>
2026-05-05 22:57:29 -0600

    feat: Entidades repositorio

--- Laboratorio_2/src/main/java/com/example/laboratorio_2/service/ProductService.java
+++
@@ -1,7 +0,0 @@
-package com.example.laboratorio_2.service;
-
-import com.example.laboratorio_2.domain.enttity.Product;
-
-public interface ProductService {
-    void createProduct(Product product);
-}
