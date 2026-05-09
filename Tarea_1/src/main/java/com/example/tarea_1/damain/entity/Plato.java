035a8bc96c91fe8c95665dd9b7cddaee6e27ac74
AstridVides-cyber <vkirito67@gmail.com>
2026-05-09 17:18:20 -0600

    feat: Entidad de plato

---
+++ Tarea_1/src/main/java/com/example/tarea_1/damain/entity/Plato.java
@@ -0,0 +1,22 @@
+package com.example.tarea_1.damain.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "platos")
+public class Plato {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private Double precio;
+
+    @Column(nullable = false)
+    private Boolean disponible;
+}
